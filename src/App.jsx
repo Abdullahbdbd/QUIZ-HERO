1e09278732ff9c9ba5898549f9e057cdb0921a9a
Abdullahbdbd <abdullahmohammad262000@gmail.com>
2023-03-30 22:56:00 +0600

    'Header'

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,14 @@ function App() {
 
   return (
     <div className="App">
-      <h1></h1>
+      <div className='container flex justify-between items-center ml-20 mt-10'>
+      <h1 className=' text-3xl font-bold'>Knowledge Store</h1>
+      <a href="">Menu</a>
+      <a href="">About</a>
+      <a href="">Help</a>
+      <img className='h-20 rounded-full' src="https://bdun.org/wp-content/uploads/2020/03/HPM.jpg" alt="" />
+      </div>
+      <hr className='container ml-20 mt-6'/>
     </div>
   )
 }
